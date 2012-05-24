cfef8406331410386e4fe2f385983e7ec04c0131
Akshay Chugh <chughakshay16@gmail.com>
2012-05-25 00:05:24 +0530

    small changes in ConferenceHooks.php

--- ConferenceHooks.php
+++ ConferenceHooks.php
@@ -5,7 +5,8 @@ class ConferenceHooks
 	{
 		$parser->setHook('conference','Conference::render');
 		$parser->setHook('conference-page','ConferencePage::render');
-		$parser->setHook('speaker','ConferenceAuthor::render');
+		$parser->setHook('author','ConferenceAuthor::render');
+		$parser->setHook('author-sub','ConferenceAuthor::renderSub');
 		$parser->setHook('submission','AuthorSubmission::render');
 		$parser->setHook('event','ConferenceEvent::render');
 		$parser->setHook('organizer','ConferenceOrganizer::render');
@@ -13,6 +14,9 @@ class ConferenceHooks
 		$parser->setHook('passport-info','ConferencePassportInfo::render');
 		$parser->setHook('registration','ConferenceRegistration::render');
 		$parser->setHook('location','EventLocation::render');
+		$parser->setHook('account', 'ConferenceAccount::render');
+		$parser->setHook('account-sub','ConferenceAccount::renderSub');
+		$parser->setHook('registration-event', 'ConferenceRegistration::renderSub')
 		return true;
 	}
 }
