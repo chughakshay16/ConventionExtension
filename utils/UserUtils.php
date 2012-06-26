9de07e73802a7b142c07c95944bea834da49055c
chughakshay16 <chughakshay16@gmail.com>
2012-06-26 22:49:07 +0530

    corrected some minor mistakes

--- utils/UserUtils.php
+++ utils/UserUtils.php
@@ -4,7 +4,7 @@ class UserUtils
 	public static function isUser($userId)
 	{
 		$dbr=wfGetDB(DB_SLAVE);
-		$row=$dbr->select('user',
+		$res=$dbr->select('user',
 		'*',
 		array('user_id'=>$userId),
 		__METHOD__,
@@ -14,7 +14,7 @@ class UserUtils
 	public static function isAccount($userId)
 	{
 		$dbr=wfGetDB(DB_SLAVE);
-		$row=$dbr->select('page_props',
+		$res=$dbr->select('page_props',
 		'*',
 		array('pp_propname'=>'account-user','pp_value'=>$userId),
 		__METHOD__,
@@ -24,16 +24,17 @@ class UserUtils
 	public static function isSpeaker($userId)
 	{
 		$dbr=wfGetDB(DB_SLAVE);
-		$row=$dbr->select('page_props',
+		$res=$dbr->select('page_props',
 		'*',
 		array('pp_propname'=>'speaker-user','pp_value'=>$userId),
 		__METHOD__,
 		array());
+		return $dbr->numRows($res)?true:false;
 	}
 	public static function isApplicant($userId)
 	{
 		$dbr=wfGetDB(DB_SLAVE);
-		$row=$dbr->select('page_props',
+		$res=$dbr->select('page_props',
 		'*',
 		array('pp_propname'=>'applicant-user','pp_value'=>$userId),
 		__METHOD__,
@@ -43,7 +44,7 @@ class UserUtils
 	public static function isOrganizer($userId)
 	{
 		$dbr=wfGetDB(DB_SLAVE);
-		$row=$dbr->select('page_props',
+		$res=$dbr->select('page_props',
 		'*',
 		array('pp_propname'=>'organizer-user','pp_value'=>$userId),
 		__METHOD__,
